01f17e5d7b7b0701a0270da0077465850e223dcc
yamajunn <amiton1115@gmail.com>
2023-05-18 14:42:50 +0900

    a

---
+++ minecraft_gray.py
@@ -0,0 +1,41 @@
+import os
+import cv2
+import numpy as np
+
+path = "./default-textures"
+files = os.listdir(path)
+
+for file in files:
+
+    base, ext = os.path.splitext(file)
+    omission_list = ["wool", "water", "lava", "planks", "log", "sponge", "hardened", "glass", "end_stone", "emerald_block", "diamond_block", "bed", "ladder", "tnt"]
+
+    if file != ".DS_Store" and ext == ".png" and not file in omission_list:
+
+        img = cv2.imread(f'default-textures/{file}', flags = cv2.IMREAD_UNCHANGED)
+
+        h, w = img.shape[:2]
+        r_count = 0
+        g_count = 0
+        b_count = 0
+        count = 0
+
+        for i in range(h):
+            for j in range(w):
+                b, g, r, a = img[i, j]
+                img[i, j] = int((b+g+r)/3)+50, int((b+g+r)/3)+50, int((b+g+r)/3)+50, a
+        # r_average = 0
+        # g_average = 0
+        # b_average = 0
+
+        # if count != 0:
+        #     r_average = int(r_count/count)
+        #     g_average = int(g_count/count)
+        #     b_average = int(b_count/count)
+
+        # for i in range(h):
+        #     for j in range(w):
+        #         b, g, r, a = img[i, j]
+        #         img[i, j] = b_average, g_average, r_average, a
+
+        cv2.imwrite(f'blocks/{file}',img)
---
+++ minecraft_monochromatic.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import numpy as np
+
+path = "./default-textures"
+files = os.listdir(path)
+
+for file in files:
+
+    base, ext = os.path.splitext(file)
+
+    if file != ".DS_Store" and ext == ".png":
+
+        img = cv2.imread(f'default-textures/{file}', flags = cv2.IMREAD_UNCHANGED)
+
+        h, w = img.shape[:2]
+        r_count = 0
+        g_count = 0
+        b_count = 0
+        count = 0
+
+        for i in range(h):
+            for j in range(w):
+                b, g, r, a = img[i, j]
+                if a != 0:
+                    r_count += r
+                    g_count += g
+                    b_count += b
+                    count += 1
+        
+        r_average = 0
+        g_average = 0
+        b_average = 0
+
+        if count != 0:
+            r_average = int(r_count/count)
+            g_average = int(g_count/count)
+            b_average = int(b_count/count)
+
+        for i in range(h):
+            for j in range(w):
+                b, g, r, a = img[i, j]
+                img[i, j] = b_average, g_average, r_average, a
+
+        cv2.imwrite(f'blocks/{file}',img)
